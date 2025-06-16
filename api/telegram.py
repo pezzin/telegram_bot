915b3c3c74eed1ce6e19907fae21e211c503e749
pezzin <christian.pezzin@gmail.com>
2025-06-16 18:33:54 +0200

    Update

--- api/telegram.py
+++ api/telegram.py
@@ -4,7 +4,7 @@ import openai
 import os
 
 # Carica token
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Setup
